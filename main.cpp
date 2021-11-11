dec62fdde23804c290d8cfe6b4e7a081f3a8f5ab
5200461 <91668775+5200461@users.noreply.github.com>
2021-11-11 13:15:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
    int n=0, a=0, b=1, c=0;
     cin>>n;
     if(n>=2){
-        cout<<b<<endl;
+        cout<<1<<endl;
         for(int i=2;i<=n;i++){
             c=a+b;
             cout<<c<<endl;
