be778e0c6db71a898091a06a15b4a880ec39fc69
5200461 <91668775+5200461@users.noreply.github.com>
2021-11-14 15:20:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,19 @@
 using namespace std;
 
 int main() {
-   int n=0, a=0, b=1, c=0;
+    int n=0, precedente=0, successivo=1, risultato=0;
     cin>>n;
     if(n>=2){
         cout<<1<<endl;
         for(int i=2;i<=n;i++){
-            c=a+b;
-            cout<<c<<endl;
-            a=b;
-            b=c;
+            risultato= precedente+successivo;
+            cout<<risultato<<endl;
+            precedente=successivo;
+            successivo=risultato;
         }
-    }else{
+    }
+    else{
         cout<<"errore"<<endl;
+    }
    return 0;
 }
